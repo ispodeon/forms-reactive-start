094d958c67d988fe7d6995e25edc8427dc6eabcd
Sandaime <ispodeon@yahoo.com>
2021-05-15 20:29:44 -0400

    got access gto controls, initializing controls reactive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   genders = ['male', 'female'];
+  signupForm: FormGroup;
+
+  ngOnInit(){
+    this.signupForm = new FormGroup({
+      // 'username': new FormControl('Ryan')  // could be set to the value you want to show initially
+      'username': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'gender': new FormControl('male')
+    });
+  }
+
+  onSubmit(){
+    console.log(this.signupForm);
+  }
 }
